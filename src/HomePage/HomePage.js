0e90c0a7fb6dd5d527f2937bae18917d5b18d45a
Borys Yudin <borysyudindm@gmail.com>
2018-10-07 21:28:39 +0300

    Add google autocomplete

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,9 +1,53 @@
 import React from "react";
 
-const HomePage = () => (
+import {withStyles} from '@material-ui/core/styles';
+
+import Autocomplete from 'react-google-autocomplete';
+
+
+const styles = {
+    input: {
+        borderRadius: 4,
+        backgroundColor: "#fff",
+        border: '1px solid #ced4da',
+        fontSize: 16,
+        padding: '8px 12px',
+        fontFamily: [
+            '-apple-system',
+            'BlinkMacSystemFont',
+            '"Segoe UI"',
+            'Roboto',
+            '"Helvetica Neue"',
+            'Arial',
+            'sans-serif',
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"',
+        ].join(','),
+    }
+};
+
+const HomePage = ({ classes }) => (
     <div>
-        <h1>Hello world</h1>
+        <Autocomplete
+            placeholder="From"
+            className={classes.input}
+            onPlaceSelected={(place) => {
+                console.log(place);
+            }}
+            types={['address']}
+            componentRestrictions={{country: "ua"}}
+        />
+        <Autocomplete
+            placeholder="To"
+            className={classes.input}
+            onPlaceSelected={(place) => {
+                console.log(place);
+            }}
+            types={['address']}
+            componentRestrictions={{country: "ua"}}
+        />
     </div>
 );
 
-export default HomePage;
+export default withStyles(styles)(HomePage);
